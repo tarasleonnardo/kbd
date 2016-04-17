5428631d14cf75333a2e74f10191cf0e0deac26b
Taras Chornenkyi <taraschrt06@gmail.com>
2016-04-17 22:48:43 +0300

    fsdfsd

--- main.cpp
+++ main.cpp
@@ -1,59 +1,21 @@
-#include "keys_table.h"
 #include "stdio.h"
-
-#include "input-event-codes.h"
-
-#define EVENTS_NUM	100
 
 int main(int argc, char* argv[])
 {
-	int cnt = 0;
-	char cp = 0;
-	char* fn = NULL;
-
-	printf("Type and press enter\n");
-
-	while (1)
-	{
-		cp = getchar();
-		putchar(cp);
-	}
+	char c = 0;
 
 	if (argc > 1 && argv[1] != NULL)
 	{
-		fn = argv[1];
-	}
-	else
-	{
-		printf("No filename found\n");
-		getchar();
-		return 0;
+		printf("Input redirection from: %s\n", argv[1]);
 	}
 
-	printf("Init Kbd\n");
-
-	if (0 == KBD_Init(fn))
-	{
-		printf("Ok\n");
-	}
-	else
-	{
-		printf("Init failed\n");
-		getchar();
-		return 0;
-	}
-
-	printf("ASCII decoding\n");
-
-	cnt = 0;
 	while (1)
 	{
-		cp = getchar();
-			putchar(cp);
+		if (EOF != (c = getchar()))
+			putchar(c);
+		else
+			printf("EOF\n");
 	}
-	printf("\n");
-	printf("Finished. Press Enter key to exit.\n");
-	getchar();
 	return 0;
 }
 
