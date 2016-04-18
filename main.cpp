944bec2ccc26202cef2c4d63f86a110743af6b57
Taras Chornenkyi <taraschrt06@gmail.com>
2016-04-18 22:13:42 +0300

    fzvbDFVDF

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 
 int main(int argc, char* argv[])
 {
+	int cnt = 0;
 	char c = 0;
 	char* path = NULL;
 	char pathBuf[128] = { 0 };
@@ -34,10 +35,15 @@ int main(int argc, char* argv[])
 
 	while (1)
 	{
-		c = getchar();// KBD_getDecodedChar();
+		c = KBD_getDecodedChar();
 		if (c != 0)
 		{
 			printf("%c", c);
+			if (++cnt > 10)
+			{
+				printf("\n");
+				cnt = 0;
+			}
 		}
 	}
 	return 0;
