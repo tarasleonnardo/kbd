7d00890d452e14b0926163ec03a0d598f0c71c5a
Taras Chornenkyi <taraschrt06@gmail.com>
2016-04-18 21:48:17 +0300

    sadfasdasdf

--- main.cpp
+++ main.cpp
@@ -34,11 +34,11 @@ int main(int argc, char* argv[])
 
 	while (1)
 	{
-		printf("");
+		//printf("c =");
 		c = KBD_getDecodedChar();
 		if (c != 0)
 		{
-			printf("%c", c);
+			printf("c = %c", c);
 		}
 		else
 		{
