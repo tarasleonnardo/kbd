1c73eaf167d54846cc6e0c1768a40c097027ea59
Taras Chornenkyi <taraschrt06@gmail.com>
2016-04-18 20:59:43 +0300

    fdgsdfg

--- keys_table.c
+++ keys_table.c
@@ -47,6 +47,7 @@ char KBD_getDecodedChar()
 		return 0;
 
 	//printf("Val = %d, Code = %d, Type = %d\n", inEvent.value, inEvent.code, inEvent.type);
+	printf("");
 	if ((inEvent.type == EV_KEY) &&
 		(inEvent.value == 0x01))// || (inEvent.value == 0x02)))
 	{
